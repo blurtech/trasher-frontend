d5ffccd318b74f375268bb6d09a8c3dce1dc0349
MacOSO <auroragamesproject@gmail.com>
2019-09-28 00:42:35 +0300

    Added onClick in registration

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,18 +2,26 @@ import React from 'react';
 import { Button, TextField } from '@material-ui/core';
 import styles from './Auth.module.scss';
 import { IUser } from '../classes/models/IUser';
+import { register, auth } from '../classes/services/api/UserApi';
 
 const Auth = () => {
   const [user, setUser] = React.useState<IUser | undefined>(undefined);
+  const [message, setMessage] = React.useState<string | undefined>(undefined);
 
   const handleChange = (event: any) => {
-      setUser({
-          [event.target.name]: event.target.value
-      });
-  }
+    user
+      ? setUser({
+          ...user,
+          [event.target.name]: event.target.value,
+        })
+      : setUser({
+          [event.target.name]: event.target.value,
+        });
+  };
 
   return (
     <div className={styles.AuthForm}>
+      {message && <div className={styles.AuthMessage}>{message}</div>}
       <TextField
         name="username"
         label="Имя пользователя"
@@ -30,10 +38,30 @@ const Auth = () => {
         onChange={handleChange}
       />
       <div className={styles.AuthButtons}>
-        <Button variant="contained" className={styles.button}>
+        <Button
+          variant="contained"
+          className={styles.button}
+          onClick={() => {
+            console.log(user);
+            setMessage(undefined);
+            user && user.username && user.password
+              ? auth(user).then(result => console.log(result))
+              : setMessage('Empty username or password');
+          }}
+        >
           Вход
         </Button>
-        <Button variant="contained" className={styles.button}>
+        <Button
+          variant="contained"
+          className={styles.button}
+          onClick={() => {
+            console.log(user);
+            setMessage(undefined);
+            user && user.username && user.password
+              ? register(user).then(result => console.log(result))
+              : setMessage('Empty username or password');
+          }}
+        >
           Регистрация
         </Button>
       </div>
