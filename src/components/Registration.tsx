9f7e3701e5948b3e740be76357ba5d1f83f88e3c
puzovoz <puzovoz@gmail.com>
2019-09-27 23:33:36 +0300

    Added component Registrationj

---
+++ src/components/Registration.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import styles from './Registration.module.scss';
+
+const Registration = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+
+
+export default Registration;
