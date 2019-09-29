2bff68dd7b637bdb5c0c19f957e58c399d4d357f
puzovoz <puzovoz@gmail.com>
2019-09-29 17:32:47 +0500

    close drawer

--- src/components/AdminPanel/AdminPanel.tsx
+++ src/components/AdminPanel/AdminPanel.tsx
@@ -4,6 +4,10 @@ import Map from '../Map/Map';
 import Menu from '../Menu/Menu';
 import { ILitterStorage } from '../../classes/models/ILitterStorage';
 import LitterStorageStoreService from "../../classes/services/LitterStorageStoreService";
+import styles from './AdminPanel.module.scss';
+import Icon from '@iconify/react';
+import chevronLeft from '@iconify/icons-mdi/chevron-left';
+import chevronRight from '@iconify/icons-mdi/chevron-right';
 
 interface IProps {
   currentUser: IUser;
@@ -13,6 +17,7 @@ interface IProps {
 const AdminPanel = (props: IProps | any) => {
   const [user, setUser] = React.useState<IUser | undefined>(undefined);
   const [pointData, setPointData] = React.useState<any>(undefined);
+  const [open, setOpen] = React.useState<boolean>(true);
   const [litterStorages, setLitterStorages] = React.useState<ILitterStorage[]>(
     []
   );
@@ -29,13 +34,23 @@ const AdminPanel = (props: IProps | any) => {
         setLitterStorages(data.items);
       });
   }, [user]);
+
+  const rerender = () => {
+    setOpen(!open)
+  }
   return (
     <div style={{
       width: document.documentElement.clientWidth,
       height: document.documentElement.clientHeight-20,
       margin: -8
     }}>
-      <Menu pointData={pointData} user={user} logout={props.logout}/>
+      <Menu open={open} pointData={pointData} user={user} logout={props.logout}/>
+      <div
+          className={styles['button']}
+          onClick={rerender}
+        >
+          {<Icon icon={chevronRight} />}
+        </div>
       {litterStorages.length > 0 && (
         <Map city={user && user.city} points={litterStorages} setPoint={(data:any) => {setPointData(data)}} />
       )}
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -24,28 +24,26 @@ interface IProps {
     user?: IUser
     pointData?: any
     logout?: any
+    open: boolean
 }
 
 const Menu = (props: IProps) => {
-  const [open, setOpen] = React.useState<boolean>(true);
+  const [open, setOpen] = React.useState<boolean>(props.open);
   const [user, setUser] = React.useState<IUser | undefined>(undefined);
 
   React.useEffect(() => {
     setUser(props.user);
   }, [props.user]);
 
-  const handleOpen = () => setOpen(prevState => !prevState);
+  React.useEffect(() => {
+    setOpen(true)
+  }, [props.open]);
+
+  const handleOpen = (where:boolean) => setOpen(where);
 
   return (
-    <div className={open ? styles.fullMenu : styles['fullMenu--closed']}>
-      <div className={open ? styles.collapseButtonOpen : styles.collapseButton}>
-        <div
-          className={open ? styles.buttonOpen : styles.button}
-          onClick={handleOpen}
-        >
-          {open ? <Icon icon={chevronLeft} /> : <Icon icon={chevronRight} />}
-        </div>
-      </div>
+    <div>
+       
       <div className={open ? styles.menuOpen : styles.menu}>
         <Drawer
           className={styles.drawer}
@@ -57,6 +55,7 @@ const Menu = (props: IProps) => {
           }}
         >
           <div className={styles.drawerHeader}>
+            <div className={styles.headerMenu}>
             <Paper className={styles.root}>
               <InputBase
                 className={styles.input}
@@ -79,6 +78,10 @@ const Menu = (props: IProps) => {
                 Log out
               </Button>
             </Paper>
+            <div className={styles.backBTN} onClick={()=>handleOpen(false)}><Icon icon={chevronLeft} /></div>
+            </div>
+            
+
           </div>
           <Divider />
           <List>
